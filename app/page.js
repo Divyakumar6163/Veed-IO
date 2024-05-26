5f5d9b47e188ec887758f88bfd63921d85278584
Divyakumar6163 <divyakumar768800@gmail.com>
2024-05-27 00:04:46 +0530

    File name chnaged

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { MantineProvider } from "@mantine/core";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/Navbar";
 import Timeline from "./components/TimeLine";
 const App = () => {
   return (
